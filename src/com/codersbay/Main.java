4182bdc865181d85d4bae43d03f58d400339fd09
Mario Wagner <ma_wa@gmx.at>
2020-03-02 09:48:13 +0100

    Optimized Code

--- src/com/codersbay/Main.java
+++ src/com/codersbay/Main.java
@@ -1,5 +1,6 @@
 package com.codersbay;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
@@ -11,30 +12,33 @@ public class Main {
 
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = randomNumber.nextInt(1000000);
-            // System.out.println(numbers[i]);
+            System.out.println(numbers[i]);
         }
         System.out.println("Divide and conquer");
 
-        System.out.println("result " + divideAndConquer(numbers));
-
-
+        System.out.println("maximum = " + divideAndConquer(numbers));
     }
 
     public static int divideAndConquer(int[] array) {
 
         if (array.length > 2) {
             System.out.println();
-            int[] firstArray = new int[array.length / 2];
-            int[] secondArray = new int[(array.length / 2) + 1];  //+1 -> for the odd case
+            System.out.println(Arrays.toString(array));
             // even or odd query
             int modulo = array.length % 2;
+            int[] firstArray = new int[array.length / 2];
+            int[] secondArray = new int[(array.length / 2)];  //-> for the even case
+
             //System.out.println(modulo);
             if (modulo == 0) {
+
                 for (int i = 0, j = array.length / 2; i < (array.length / 2); i++, j++) {
                     firstArray[i] = array[i];
                     secondArray[i] = array[j];
                 }
             } else {
+                int[] temp = new int[(array.length / 2) + 1]; //+1 -> for the odd case
+                secondArray = temp;
                 for (int i = 0; i < (array.length / 2); i++) {
                     firstArray[i] = array[i];
                 }
@@ -42,52 +46,21 @@ public class Main {
                     secondArray[i] = array[j];
                 }
             }
-
-         /*   System.out.println("array.length " + array.length);
-            System.out.println("array " + Arrays.toString(array));
             System.out.println("first " + Arrays.toString(firstArray));
+            int maxLeft = divideAndConquer(firstArray);
             System.out.println("second " + Arrays.toString(secondArray));
-            int firstMaximum = firstArray[0];
+            int maxRight = divideAndConquer(secondArray);
 
-            for (int number : firstArray) {
-                if (number > firstMaximum) {
-                    firstMaximum = number;
-                }
-            }
-            System.out.println("recursive firstmax " + firstMaximum);
-
-
-            int secondMaximum = secondArray[0];
-
-            for (int number2 : secondArray) {
-                if (number2 > secondMaximum) {
-                    secondMaximum = number2;
-                }
-            }
-            System.out.println("recursive secondmax " + secondMaximum);*/
-
-            divideAndConquer(firstArray);
-            divideAndConquer(secondArray);
-
-            //  int maximum = firstMaximum > secondMaximum ? firstMaximum : secondMaximum;
-
-            // return 0;
-
-            return maximum;
+            return Math.max(maxLeft, maxRight);
 
         } else if (array.length == 2) {
-            System.out.println("else if " + (Math.max(array[0], array[1])));
-            if (Math.max(array[0], array[1]) > maximum) {
-                maximum = Math.max(array[0], array[1]);
-            }
-
-            return maximum;
+            System.out.println("array.length == 2 " + (Math.max(array[0], array[1])));
+            return Math.max(array[0], array[1]);
+        } else if (array.length == 1) {
+            System.out.println("array.length == 1 " + array[0]);
+            return array[0];
         } else {
-            System.out.println("else " + array[0]);
-            if (array[0] > maximum) {
-                maximum = array[0];
-            }
-            return maximum;
+            throw new IllegalArgumentException("this is not a legal Argument");
         }
 
     }
